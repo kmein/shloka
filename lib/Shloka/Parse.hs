ae5e2cac1b7bef8b0e326c9d9895decdac432cf6
Kierán Meinhardt <kmein@posteo.de>
2022-11-02 17:43:33 +0100

    fix: allow complex word onsets

--- lib/Shloka/Parse.hs
+++ lib/Shloka/Parse.hs
@@ -35,16 +35,19 @@ parseLine = do
     location1 <- read <$> count 2 digitChar
     location2 <- read <$> count 3 digitChar
     location3 <- read <$> count 3 digitChar
-    let lineLocation = (location1, location2, location3)
-    lineType <-
-        (letterChar <|> char ' ') >>= \case
-            ' ' -> pure Heading
-            c
-                | isLower c -> pure $ Verse c
-                | isUpper c -> pure $ Prose c
-                | otherwise -> fail $ "Unexpected line type: " ++ show c
+    lineTypeChar <- letterChar <|> char ' '
+    lineType <- case lineTypeChar of
+        ' ' -> pure Heading
+        c
+            | isLower c -> pure Verse
+            | isUpper c -> pure Prose
+            | otherwise -> fail $ "Unexpected line type: " ++ show c
+    let lineLocation = (location1, location2, location3, if lineTypeChar == ' ' then Nothing else Just lineTypeChar)
     _ <- char ' '
     lineText <- Text.pack <$> many harvardKyoto
     pure $ Line{..}
   where
-    harvardKyoto = letterChar <|> char ' ' <|> char '\'' <|> char ';' <|> char '&' <|> char ':'
+    harvardKyoto =
+        letterChar
+            <|> oneOf (" '-;&:" :: [Char])
+            <|> (' ' <$ (digitChar <|> oneOf ("[]=" :: [Char])))
--- lib/Shloka/Syllable.hs
+++ lib/Shloka/Syllable.hs
@@ -5,16 +5,30 @@ module Shloka.Syllable where
 import Data.Maybe (maybeToList)
 import Data.Void (Void)
 import Shloka.Token
-import Text.Megaparsec (Parsec, eof, many, oneOf, optional, parse, someTill, (<|>))
+import Text.Megaparsec (Parsec, eof, many, oneOf, optional, parse, single, some, try, (<|>))
+import qualified Text.Megaparsec (Token)
+import qualified Text.Megaparsec.Stream
+
+singles :: Text.Megaparsec.Stream.Stream s => [Text.Megaparsec.Token s] -> Parsec Void s [Text.Megaparsec.Token s]
+singles xs = xs <$ mapM single xs
 
 -- TODO we may want to throw away initial "s" here: skandhaH gets parsed as if it were kandhaH
 -- this has no effect on the metre, as it is only thrown away if no words precedes it.
 -- if a word precedes skandhaH, e. g. tasya skandhaH, it will be syllabified as tas.yas.kan.dhaH,
 -- which is good enough for metrical purposes
 syllabify :: [Token] -> [[Token]]
-syllabify toks = case parse (syllable `someTill` eof) "" (reverse toks) of
-    Right syls -> reverse syls
-    Left e -> error $ show e
+syllabify toks = either (error . show) id $ parse withInitialConsonant "" (reverse toks)
+  where
+    withInitialConsonant = do
+        syls <- some (try syllable)
+        initialConsonants <- many $ oneOf consonantTokens
+        eof
+        pure $ onHead (initialConsonants ++) (reverse syls)
+
+onHead :: (a -> a) -> [a] -> [a]
+onHead f = \case
+    [] -> []
+    (x : xs) -> f x : xs
 
 -- parse in reverse: figure out syllables from right to left
 syllable :: Parsec Void [Token] [Token]
@@ -23,3 +37,20 @@ syllable = do
     nucleus <- oneOf $ longVowelTokens <|> shortVowelTokens
     onset <- optional $ oneOf consonantTokens
     return $ maybeToList onset ++ [nucleus] ++ reverse coda
+
+{-
+
+          try
+              ( try (singles ["r", "p"])
+                  <|> try (singles ["r", "z"])
+                  <|> try (singles ["r", "b"])
+                  <|> try (singles ["r", "t"])
+                  <|> try (singles ["y", "v"])
+                  <|> try (singles ["S", "k"])
+                  <|> try (singles ["k", "s"])
+                  <|> try (singles ["v", "s"])
+                  <|> try (singles ["y", "n"])
+                  <|> singles ["v", "d"]
+              )
+              <|> ((: []) <$> oneOf consonantTokens)
+  -}
