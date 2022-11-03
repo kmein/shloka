639bbf1b2c03bbc68904b1c6b1a839b54f6256ab
Kierán Meinhardt <kmein@posteo.de>
2022-11-03 19:42:03 +0100

    feat: localisation program

---
+++ app-localisation/Main.hs
@@ -0,0 +1,62 @@
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE OverloadedLists #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TypeApplications #-}
+
+module Main where
+
+import qualified Data.Text as Text
+
+import Control.Monad (forM_)
+import qualified Data.ByteString.Lazy as ByteString
+import Data.Csv
+import Data.Either (rights)
+import Data.Map (Map)
+import Data.Text (Text, pack)
+import Shloka
+import Shloka.Import
+import Shloka.Metre (Metre (Shloka), scanSyllable)
+import Shloka.Parse (Line (..), LineType (Verse))
+import Shloka.Syllable (syllabify)
+import Shloka.Token (tokenize)
+
+csvColumns :: Header
+csvColumns = ["parva", "sub_parva", "verse", "sub_verse", "word", "lengths", "position"]
+
+localiseWords :: Line -> [Map Text Text]
+localiseWords line = go 1 [] $ Text.words $ lineText line
+  where
+    go currentPosition acc = \case
+        [] -> reverse acc
+        word : restWords ->
+            let syllables = syllabify $ tokenize $ Text.replace "'" "" word
+                (parva, subParva, verse, subVerse) = lineLocation line
+             in go
+                    (currentPosition + length syllables)
+                    ( ( [ ("parva", pack $ show parva)
+                        , ("sub_parva", pack $ show subParva)
+                        , ("verse", pack $ show verse)
+                        , ("sub_verse", maybe Text.empty Text.singleton subVerse)
+                        , ("word", word)
+                        , ("lengths", pack $ show $ map scanSyllable syllables)
+                        , ("position", pack $ show currentPosition)
+                        ] ::
+                            Map Text Text
+                      ) :
+                      acc
+                    )
+                    restWords
+
+main :: IO ()
+main =
+    forM_ @[] [Mahabharata] $ \epic ->
+        forM_ @[] [1 .. kandaCount epic] $ \kanda -> do
+            kandaLines <- rights <$> readKanda epic kanda
+            ByteString.putStr $
+                encodeByName csvColumns $
+                    concatMap localiseWords $
+                        filter isShloka $ kandaLines
+  where
+    isShloka line = lineType line == Verse && metre == Just Shloka
+      where
+        (_, _, metre) = scanVerse $ lineText line
